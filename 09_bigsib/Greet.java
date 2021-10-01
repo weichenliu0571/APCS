ca7e655f1bc6665df8d498dc55486980ded60aa0
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-10-01 12:27:42 -0400

    Update Greet.java

--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -3,7 +3,15 @@ Giant Pandas (Brian Li, Melody Lew, Weichen Liu)
 APCS
 HW09 -- What Are BigSibs Good For?
 2021-10-01
+
+DISCOVERIES
+- 
+UNRESOLVED QUESTIONS
+- 
+
+
 */
+
 
 public class Greet {
 	public static void main(String[] args) {
