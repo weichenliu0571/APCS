9d010e4e528171414d0bd8e311bab291a3c792f0
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-10-04 16:59:31 -0400

    Update Greet.java

--- 10_bigsib/Greet.java
+++ 10_bigsib/Greet.java
@@ -3,11 +3,12 @@ Giant Pandas (Brian Li, Melody Lew, Weichen Liu, Robert, Blueface, Ollie)
 APCS
 HW10 -- Refactor Big Sib One
 2021-10-04
+
 DISCOVERIES
-- 
+ 0. By changing the "void" in a method to "String", we are indicating what type of value we want this method to return. If it returns void, then nothing will be returned in the event that this method is called. 
     
 UNRESOLVED QUESTIONS
-- 
+ 0. In the case that we are returning a String, like "17", and want to turn it into an integer, how would we do that?
 */
 
 public class Greet {
