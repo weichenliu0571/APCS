58ef674724e168b2f0a324e3be61f753edda654e
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-10-04 16:57:41 -0400

    Update Greet.java

--- 10_bigsib/Greet.java
+++ 10_bigsib/Greet.java
@@ -1,1 +1,20 @@
+/*
+Giant Pandas (Brian Li, Melody Lew, Weichen Liu, Robert, Blueface, Ollie)
+APCS
+HW10 -- Refactor Big Sib One
+2021-10-04
+DISCOVERIES
+- 
+    
+UNRESOLVED QUESTIONS
+- 
+*/
+
+public class Greet {
+  public static void main(String[] args) {
+    System.out.println( BigSib.greet("Crosby") );
+    System.out.println( BigSib.greet("Stills") );
+    System.out.println( BigSib.greet("Nash") );
+  }
+}
 
