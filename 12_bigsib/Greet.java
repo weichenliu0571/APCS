9ea0b0177b740b9508c726ccfebd1f027d7c1b23
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-10-05 18:33:03 -0400

    Update Greet.java

--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -3,8 +3,9 @@ Giant Pandas (Brian Li, Melody Lew, Weichen Liu, Robert, Blueface, Ollie)
 APCS
 HW12 -- On Elder Individuality and the Elimination of Radio Fuzz
 2021-10-05
+
 DISCOVERIES
- 0. 
+ 0. If the method is static, the instance variable that is used everytime will be the last instance variable that is set. 
     
 UNRESOLVED QUESTIONS
  0. 
