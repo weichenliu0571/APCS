5d2743386f47b386243e52a149249e3bcbf92c33
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-10-05 17:21:34 -0400

    Update Greet.java

--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -1,1 +1,24 @@
+/*
+Giant Pandas (Brian Li, Melody Lew, Weichen Liu, Robert, Blueface, Ollie)
+APCS
+HW12 -- On Elder Individuality and the Elimination of Radio Fuzz
+2021-10-05
+DISCOVERIES
+ 0. 
+    
+UNRESOLVED QUESTIONS
+ 0. 
+*/
 
+public class Greet {
+  public static void main(String[] args) {
+    String greeting;
+    
+    BigSib.richard = new.BigSib();
+    richard.setHelloMsg( "Word up" );
+    
+    greeting = richard.greet( "freshman" );
+    System.out.println( greeting )
+      
+  } // end main()
+} // end Greet
