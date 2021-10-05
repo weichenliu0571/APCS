69fc3f719c6ad862d7780abc697cd77818d8a833
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-10-05 18:22:46 -0400

    Update Greet.java

--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -14,11 +14,23 @@ public class Greet {
   public static void main(String[] args) {
     String greeting;
     
-    BigSib richard = new BigSib();
-    richard.setHelloMsg( "Word up" );
+    BigSib richard = new BigSib(); // creates an object called richard whose class is BigSib
+    richard.setHelloMsg( "Word up" ); // the instance variable "HelloMsg" in the object richard is set to "Word Up" 
+    BigSib johnson = new BigSib();
+    johnson.setHelloMsg( "Salutations" );
+    BigSib jacob = new BigSib();
+    jacob.setHelloMsg( "Hey ya");
+    BigSib justin = new BigSib();
+    justin.setHelloMsg( "Sup");
     
-    greeting = richard.greet( "freshman" );
+    greeting = richard.greet( "freshman" ); // The String greeting is set to = to the String that is returned when the method greet in the object richard is invoked. 
+    System.out.println( greeting ); // Just prints the string
+    greeting = johnson.greet( "Dr. Spaceman");
     System.out.println( greeting );
-      
+    greeting = jacob.greet( "Fooey" );
+    System.out.println( greeting );
+    greeting = justin.greet( "mom");
+    System.out.println( greeting );
+   
   } // end main()
 } // end Greet
