9a3329c52dcce92562b1a7e4df4ca611b5205ccb
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-10-05 17:22:15 -0400

    Update BigSib.java

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -1,1 +1,13 @@
+/*
+Giant Pandas (Brian Li, Melody Lew, Weichen Liu, Robert, Blueface, Ollie)
+APCS
+HW12 -- On Elder Individuality and the Elimination of Radio Fuzz
+2021-10-05
+DISCOVERIES
+ 0. 
+    
+UNRESOLVED QUESTIONS
+ 0. 
+*/
+
 
