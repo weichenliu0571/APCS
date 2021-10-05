b3e86bb12967831557e328c9f3945e5cc1590ee0
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-10-05 18:35:50 -0400

    Update BigSib.java

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -5,10 +5,11 @@ HW12 -- On Elder Individuality and the Elimination of Radio Fuzz
 2021-10-05
 
 DISCOVERIES
- 0. If the method is static, the instance variable that is used everytime will be the last instance variable that is set. 
+ 0. If the methods in BigSib.java are static, the instance variable that is used everytime will be the last instance variable that is set. 
+ 1. The instance variable for each of these objects is different.
     
 UNRESOLVED QUESTIONS
- 0. 
+ 0. What else can we do with these instance variables?
 */
 
 public class BigSib {
