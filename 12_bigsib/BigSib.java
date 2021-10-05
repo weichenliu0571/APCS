8c9901ce988e9a395512c482b2e181e4ec405bc9
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-10-05 18:39:45 -0400

    Update BigSib.java

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -7,16 +7,18 @@ HW12 -- On Elder Individuality and the Elimination of Radio Fuzz
 DISCOVERIES
  0. If the methods in BigSib.java are static, the instance variable that is used everytime will be the last instance variable that is set. 
  1. The instance variable for each of these objects is different.
+ 2. We need to declare the instance variable before we can use it in the other methods
     
 UNRESOLVED QUESTIONS
  0. What else can we do with these instance variables?
 */
 
 public class BigSib {
- 
+
+ public String helloMsg; // declaring instance variable
  
  public String setHelloMsg(String x) {
-  String helloMsg = x;
+  helloMsg = x;
   return helloMsg;
  }
  
