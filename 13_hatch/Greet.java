abc8b0701a08a7584e6ba0c77bc542bb9afa7f97
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-10-06 18:45:47 -0400

    Update Greet.java

--- 13_hatch/Greet.java
+++ 13_hatch/Greet.java
@@ -1,13 +1,14 @@
 /*
 Giant Rabbit (Brian Wang, Weichen Liu, Blueface, Dolphin)
 APCS
-HW12 -- Where do BigSibs Come From?
+HW13 -- Where do BigSibs Come From?
 2021-10-06
 DISCOVERIES
- 0. 
+ 0. The constructor removes the need for the method setHelloMsg. 
+ 1. Instead of needing to use another line to invoke setHelloMsg, the constructor runs immediatly after you create a new object. It runs when new is called.
     
 UNRESOLVED QUESTIONS
- 0. 
+ 0. What else can we do with constructors?
 */
 
 public class Greet {
