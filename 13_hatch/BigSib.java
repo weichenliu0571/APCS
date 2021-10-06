d238f3f68391cc84ce45c660744f517214a603b8
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-10-06 18:22:52 -0400

    Update BigSib.java

--- 13_hatch/BigSib.java
+++ 13_hatch/BigSib.java
@@ -1,1 +1,25 @@
+/*
+Giant Rabbit (Brian Wang, Weichen Liu, Blueface, Dolphin)
+APCS
+HW12 -- On Elder Individuality and the Elimination of Radio Fuzz
+2021-10-06
+DISCOVERIES
+ 0. 
+    
+UNRESOLVED QUESTIONS
+ 0. 
+*/
 
+public class BigSib {
+  //instance vars
+  Private String helloMsg;
+  
+  public BigSib( String Msg ) {
+    helloMsg = Msg;
+  }
+  
+  //methods
+  public void setHelloMsg( String newMsg ) {
+    return helloMsg + " " + name;
+  }
+}
