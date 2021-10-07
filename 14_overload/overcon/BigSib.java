3fa174cffe90e6d760d77b71dcb13957f0745090
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-10-07 18:58:36 -0400

    Update BigSib.java

--- 14_overload/overcon/BigSib.java
+++ 14_overload/overcon/BigSib.java
@@ -1,3 +1,16 @@
+/*
+Giant Pigs (Melody Lew, Brianna Tieu, Weichen Liu, Blueface, Pepe, Ollie)
+APCS
+HW14 -- Customize Your Creation
+2021-10-07
+DISCOVERIES
+ 0. Overloaded constructors allow us to directly edit the instance variable in the object by just changing the argument on the line that creates the object. 
+ 1. Overloaded constructors are convenient; it cuts down on the amount of coding we need to do.
+    
+UNRESOLVED QUESTIONS
+ 0. Is there a way to only use the default constructor to get all the different outputs we need. 
+*/
+
 public class BigSib {
   String helloMsg;
   
