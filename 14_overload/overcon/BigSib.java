5c315ec449e3a2bbd6ebdcdbde4e7afde7d03795
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-10-07 18:44:37 -0400

    Update BigSib.java

--- 14_overload/overcon/BigSib.java
+++ 14_overload/overcon/BigSib.java
@@ -1,1 +1,15 @@
-
+public class BigSib {
+  String helloMsg;
+  
+  public BigSib() {
+    helloMsg = "Word Up";
+  }
+  
+  public BigSib(String msg) {
+    helloMsg = msg;
+  }
+  
+  public void greet(String name) {
+    return helloMsg + " " + name;
+  }
+}
