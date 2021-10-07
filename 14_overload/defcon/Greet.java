6c8b144a12e60319140af5c810abb529cb9743e5
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-10-07 18:38:38 -0400

    Update Greet.java

--- 14_overload/defcon/Greet.java
+++ 14_overload/defcon/Greet.java
@@ -1,17 +1,24 @@
 public class Greet {
   public static void main( String[] args ) {
     String greeting;
-    BigSib richard = new BigSib("Word up");
-    BigSib grizz = new BigSib("Hey ya");
-    BigSib dotCom = new BigSib("Sup");
-    BigSib tracy = new BigSib("Salutations");
+    BigSib richard = new BigSib();
+    BigSib grizz = new BigSib();
+    BigSib dotCom = new BigSib();
+    BigSib tracy = new BigSib();
     
     greeting = richard.greet("freshman");
     System.out.println(greeting);
+    
+    tracy.setHelloMsg("Salutations")
     greeting = tracy.greet("Dr. Spaceman");
     System.out.println(greeting);
+ 
+    grizz.setHelloMsg("Hey ya")
     greeting = grizz.greet("Kong Fooey");
     System.out.println(greeting);
+    
+    
+    dotCom.setHelloMsg("Sup")
     greeting = dotCom.greet("mom");
     System.out.println(greeting);
     
