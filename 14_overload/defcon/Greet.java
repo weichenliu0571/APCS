3b0c7024837dadad9242170f98d13e2ca403bc62
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-10-07 18:54:30 -0400

    Update Greet.java

--- 14_overload/defcon/Greet.java
+++ 14_overload/defcon/Greet.java
@@ -1,3 +1,17 @@
+/*
+Giant Pigs (Melody Lew, Brianna Tieu, Weichen Liu, Blueface, Pepe, Ollie)
+APCS
+HW14 -- Customize Your Creation
+2021-10-07
+DISCOVERIES
+ 0. As of now, we need the method setHelloMsg if we only have one default constructor.
+ 1. Typing the same code over and over again really helps with memorization. 
+    
+UNRESOLVED QUESTIONS
+ 0. Is there a way to only use the default constructor to get all the different outputs we need. 
+*/
+  
+  
 public class Greet {
   public static void main( String[] args ) {
     String greeting;
