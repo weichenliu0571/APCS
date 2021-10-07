28eaeca01f586fddf077c1ebbb8c50407c2ef150
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-10-07 18:58:17 -0400

    Update Greet.java

--- 14_overload/overcon/Greet.java
+++ 14_overload/overcon/Greet.java
@@ -1,9 +1,22 @@
+/*
+Giant Pigs (Melody Lew, Brianna Tieu, Weichen Liu, Blueface, Pepe, Ollie)
+APCS
+HW14 -- Customize Your Creation
+2021-10-07
+DISCOVERIES
+ 0. Overloaded constructors allow us to directly edit the instance variable in the object by just changing the argument on the line that creates the object. 
+ 1. Overloaded constructors are convenient; it cuts down on the amount of coding we need to do.
+    
+UNRESOLVED QUESTIONS
+ 0. Is there a way to only use the default constructor to get all the different outputs we need. 
+*/
+
 
 public class Greet {
   public static void main( String[] args ) {
     String greeting;
-    BigSib richard = new BigSib();
-    BigSib grizz = new BigSib("Hey ya");
+    BigSib richard = new BigSib(); // does not have parameter since the default constructor sets the instance variable to be what we need here.
+    BigSib grizz = new BigSib("Hey ya"); // the parameter changes the helloMsg to what we want.
     BigSib dotCom = new BigSib("Sup");
     BigSib tracy = new BigSib("Salutations");
     
