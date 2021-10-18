1d0b938d9a384aba0861189d567e31b676fe32f8
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-10-18 18:51:22 -0400

    Update BankAccount.java

--- 19_BankAccount/BankAccount.java
+++ 19_BankAccount/BankAccount.java
@@ -1,7 +1,18 @@
-/**
-DISCOVERIES
-- You have to return a boolean in both parts of the if else statement if you plan to make the return type of the method a boolean. 
-**/
+/*
+   Geese: Weichen Liu + Blueface, Anthony Sun + Corn, Brian Li + Robert
+   APCS pd6
+   HW19 -- adding more functionality
+   2021-10-18
+*/
+
+/*
+ * DISCO:
+ * We learned how to typecast an int to a short primitive
+ * You have to return a boolean in both parts of the if else statement if you plan to make the return type of the method a boolean.
+ 
+ * Q/C/C:
+ * Authenticate still isn't useful right now, because it cannot be used in deposit/withdraw (no params to take in accnt info)
+*/
 
 public class BankAccount {
 
@@ -29,24 +40,22 @@ public class BankAccount {
 
   public short setPin( short newPin ) {
     short oldPin = pin;
-     if (newPin >= 1000 && newPin < 9999) {
-        pin = newPin;
-     } else {
-        pin = 9999;
-        System.out.println("ERROR! Pin is invalid!");
-        }
+    if (!(newPin < (short) 9999 && newPin >= (short) 1000)) {
+	newPin = (short) 9999;
+	System.out.println("Your pin number is invalid, please try again");
+    }
+    pin = newPin;
     return oldPin;
   }
 
   public int setAcctNum( int newAcctNum ) {
     int oldAcctNum = acctNum;
-    if (newAcctNum >= 100000000 && newAcctNum < 999999999) {
-       acctNum = newAcctNum;
-    }  else {
-       acctNum = 999999999;
-       System.out.println("ERROR! Account number invalid!");
+    if (!(newAcctNum < 999999999 && newAcctNum >= 100000000)) {
+        newAcctNum = 999999999;
+    	System.out.println("Your account number is invalid, please try again.");
     }
-       return oldAcctNum;
+    acctNum = newAcctNum;
+    return oldAcctNum;
   }
 
   public double setBalance( double newBalance ) {
@@ -62,21 +71,17 @@ public class BankAccount {
   }
 
   public boolean withdraw( double withdrawAmount ) {
-    if (balance - withdrawAmount >= 0) {
+    if (balance < withdrawAmount) {
+	System.out.println("Sorry, you cannot withdraw more than your current balance.");
+	return false;
+    }
     balance = balance - withdrawAmount;
-       return true;
-    } 
-       System.out.println("ERROR! Not enough Money!");
-       return false; 
-  }
-        
-  public boolean authenticate(int givenAccNum, String givenPassword) {
-     if (acctNum == givenAccNum && passwd == givenPassword) {
-        return true;
-     } 
-        return false;
+    return true;
   }
 
+  public boolean authenticate(int accountNumber, String pass) {
+	return acctNum == accountNumber && passwd == pass;
+  }
 
   //overwritten toString()
   public String toString() {
@@ -90,40 +95,37 @@ public class BankAccount {
     return retStr;
   }
 
-//main method for testing  
-  public static void main( String[] args ) {  
-    // INSERT YOUR METHOD CALLS FOR TESTING HERE  
-    BankAccount ba = new BankAccount();  
-    ba.setName("Geese");  
-    ba.setPasswd("qwertyuiop1234mydogiscalledsnowball");   
-    ba.setPin((short) 1234);  
-    ba.setAcctNum(100000012);  
-    ba.setAcctNum(0);
-    ba.setBalance(100);  
-    System.out.println(ba.toString());  
-    System.out.println("\ndeposited 40 dollars:");  
-    ba.deposit(40);  
-    System.out.println(ba.toString());  
-    System.out.println("\nwithdrew 50 dollars:");  
-    ba.withdraw(50);  
-    System.out.println(ba.toString());  
-     
-    BankAccount bc = new BankAccount();
-    bc.setName("Sheesh");
-    bc.setPasswd("brokeme123");
-    System.out.println("\nShould not work:");
-    bc.setPin((short) 12345);
-    bc.setAcctNum(99999);
-    bc.setBalance(100);
-    System.out.println(bc.toString());
-    System.out.println(bc.authenticate(0 , "brokeme123"));
-    System.out.println("\nwithdrew 150 dollars, should not work:");  
-    bc.withdraw(150);  
-    System.out.println(bc.toString());  
+
+  //main method for testing
+  public static void main( String[] args ) {
+    // INSERT YOUR METHOD CALLS FOR TESTING HERE
+    BankAccount ba = new BankAccount();
+    ba.setName("Geese");
+    ba.setPasswd("qwertyuiop1234");
     
+    System.out.println("\nInvalid pin #:");
+    ba.setPin((short) 12345);
     
+    ba.setPin((short) 1234);
+    System.out.println("\nInvalid accnt number:");
+    ba.setAcctNum(100);
+    ba.setAcctNum(100000012);
+    ba.setBalance(100);
+    System.out.print("\nTesting valid account # and password: ");
+    System.out.println(ba.authenticate(100000012,"qwertyuiop1234"));
+    System.out.print("\nTesting invalid account # and password: ");
+    System.out.println(ba.authenticate(123,"asdf"));
+
+    System.out.println(ba.toString());
+    System.out.println("\ndeposited 40 dollars:");
+    ba.deposit(40);
+    System.out.println(ba.toString());
+    System.out.println("\nwithdrew 50 dollars:");
+    ba.withdraw(50);
+    System.out.println(ba.toString());
+
+    System.out.println("Testing invalid withdrawal - withdraw amt > balance:");
+    System.out.println(ba.withdraw(10000));
   }//end main()
 
 }//end class BankAccount
-
-
