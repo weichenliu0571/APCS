9c9915d140bbb91e0d63515de8cec9ba7251e100
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-10-23 09:41:17 -0400

    Update Driver.java

--- 23_Coin/Driver.java
+++ 23_Coin/Driver.java
@@ -12,44 +12,37 @@
 ***/
 
 public class Driver {
+    public static void main(String[] args) {
+        // build Objects from blueprint specified by class Coin
 
-  public static void main( String[] args ) {
-
-    //build Objects from blueprint specified by class Coin
-
-
-    //test default constructor
-    // /*===================TOP==========================
-      Coin mine = new Coin();
-
-      //test 1st overloaded constructor
-      Coin yours = new Coin( "quarter" );
-
-      //test 2nd overloaded constructor
-      Coin wayne = new Coin( "dollar", "heads" );
-
-      //test toString() methods of each Coin
-      System.out.println("mine: " + mine);
-      System.out.println("yours: " + yours);
-      System.out.println("wayne: " + wayne);
-
-      //test flip() method
-      System.out.println("\nAfter flipping...");
-      yours.flip();
-      wayne.flip();
-      System.out.println("yours: " + yours);
-      System.out.println("wayne: " + wayne);
-
-      //test equals() method
-      if ( yours.equals(wayne) ) {
-        System.out.println( "Matchee matchee!" );
-      }
-      else {
-        System.out.println( "No match. Firestarter you can not be." );
-      }
-
-      //====================BOTTOM======================*/
-
-  }//end main()
-
-}//end class
+        // test default constructor
+        Coin mine = new Coin();
+        
+        // test 1st overloaded constructor
+        Coin yours = new Coin("quarter");
+        
+        // test 2nd overloaded constructor
+        Coin wayne = new Coin("dollar", "heads");
+        
+        // test toString() methods of each Coin
+        System.out.println("mine: " + mine);
+        System.out.println("yours: " + yours);
+        System.out.println("wayne: " + wayne);
+        
+        // test flip() method
+        System.out.println("\nAfter flipping...");
+        yours.flip();
+        wayne.flip();
+        System.out.println("yours: " + yours);
+        System.out.println("wayne: " + wayne);
+        
+        // test equals() method
+        if (yours.equals(wayne)) {
+            System.out.println("Matchee matchee!");
+        } else {
+            System.out.println("No match. Firestarter you can not be.");
+        }
+        /*===================TOP==========================
+        ====================BOTTOM======================*/
+    }
+}
