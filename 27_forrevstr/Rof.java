255bccf95a3c6b02cc53d747bb12a7ee233b7544
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-10-28 19:51:58 -0400

    Update Rof.java

--- 27_forrevstr/Rof.java
+++ 27_forrevstr/Rof.java
@@ -10,11 +10,13 @@ public class forj {
 	public static String reverseF(String s) {
 		String result = "";
 		for(int i = s.length(); i = 0; i --;) {
-			result += s[i];
+			result += s.substring(i - 1, i - 2)
 		}
+		return result
 	}
 	
 	public static void main(String[] args) {
 		System.out.println(fenceF(1));
+		System.out.println(reverseF("stressed"));
 	}
 }
