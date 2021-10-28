668d3207cb0822d1a1b6f5a177551b8f8617ff59
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-10-28 19:12:57 -0400

    Update Rof.java

--- 27_forrevstr/Rof.java
+++ 27_forrevstr/Rof.java
@@ -1,1 +1,13 @@
-
+public class forj {
+	public static String fenceF(int posts) {
+	String result = "|";
+		for(int i = 1; i < posts; i ++) {
+			result += "--|";
+		}
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(fenceF(1));
+	}
+}
