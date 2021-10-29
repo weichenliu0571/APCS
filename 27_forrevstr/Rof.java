1d4f537e9e296cdadee2d5ab06b174aa5b50a9ee
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-10-28 20:14:36 -0400

    Update Rof.java

--- 27_forrevstr/Rof.java
+++ 27_forrevstr/Rof.java
@@ -1,6 +1,21 @@
+// Giant Squid :: 
+// APCS
+// HW27 - Using for loops to 
+// 2021-10-27
+
+/*
+DISCO:
+- We have to set the base case of the recursive method to be (a == b). 
+- If you misplace a braces, the error: class, interface or enum expected may show up.
+QCC:
+- Is there a more efficient way to replace the higher and lower numbers in gcdEW()?
+ALGO:
+- By constantly replacing the higher number in the argument list with the difference between the higher number and the smaller number, we can achieve a point where the higher number is equal to the lower number. That is when we return either number.
+*/
+
 public class Rof {
 	public static String fenceF(int posts) {
-	String result = "|";
+		String result = "|";
 		for(int i = 1; i < posts; i ++) {
 			result += "--|";
 		}
@@ -8,12 +23,16 @@ public class Rof {
 	}
 	
 	public static String reverseF(String s) {
-	String result = "";
-	int stringLength = s.length();
-		for(int i = 0; i < stringLength; i ++) {
-			result += s.substring(stringLength - (i+1), stringLength - i);
+		String result = "";
+		int stringLength = s.length();
+		for(int i = stringLength; i > 0; i --) {
+			result += s.substring(i - 1, i - 2);
 		}
 		return result;
+	}
+	
+	public static String reverseR(String s) {
+		
 	}
 	
 	public static void main(String[] args) {
