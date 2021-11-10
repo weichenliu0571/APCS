bbb8d2d33e4174ad5b8372cc568c6fcc623eeac4
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-11-09 22:02:09 -0500

    Update Pig.java

--- lab00_oink/v2/Pig.java
+++ lab00_oink/v2/Pig.java
@@ -11,10 +11,16 @@
  *
  * QCC
  * - What is the most efficient loop that goes through every word in the input that we give Scanner?
+ *
+ * HOW WE UTILIZED SCANNER DEMO (v2)
+ * - We used the while loop with the .hasNext(), but instead of using .next(), we used .nextLine() to check the entirety of the input on the line. We did this to consider inputs that have more than one word.
+ *
+ * WHAT CAUSES THE RUNTIME ERROR IN THE SCANNER DEMO
+ * - The scanner runs .next() twice on an input that only has one word on one line.
  * 
-HOW WE UTILIZED SCANNER DEMO (v<version num in which you incorporated Scanner functionality>)
-WHAT CAUSES THE RUNTIME ERROR IN THE SCANNER DEMO
-NEW IN v<this version>
+ * NEW IN v2
+ * - actual implementation of ScannerDemo
+ *
  * class Pig
  * a Pig Latin translator
  * ~~~~~~~~~~~~~~~~~~~ SKELETON ~~~~~~~~~~~~~~~~~~~
