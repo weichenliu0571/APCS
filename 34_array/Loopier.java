9d111a8f95c34a72829fde2847b8c492a044117a
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-11-13 09:51:08 -0500

    Update Loopier.java

--- 34_array/Loopier.java
+++ 34_array/Loopier.java
@@ -1,38 +1,64 @@
-import java.lang.Math;
+  import java.lang.Math;
 
-public class Loopier {
-  public static int[] arrayRandInts(int size) {
-    int[] randInts = new int[size];
-    for (int x = 0 ; x < size ; x += 1) {
-      randInts[x] = (int)(Math.random() * 10);
+  public class Loopier {
+
+    public static int[] arrayRandInts(int size) {
+      int[] randInts = new int[size];
+      for (int x = 0 ; x < size ; x += 1) {
+        randInts[x] = (int)(Math.random() * 10);
+      }
+      return randInts;
     }
-    return randInts;
-  }
-  public static String strArrayInts(int[] intArray) {
-    String out = ("{" + intArray[0]);
-    for (int x = 1 ; x < intArray.length ; x += 1) {
-      out += ", " + intArray[x];
+
+
+    public static String strArrayInts(int[] intArray) {
+      String out = ("{" + intArray[0]);
+      for (int x = 1 ; x < intArray.length ; x += 1) {
+        out += ", " + intArray[x];
+      }
+      out += "}";
+      return out;
     }
-    out += "}";
-    return out;
-  }
-  public static int linSearch(int[] a, int target) {
-    for (int x = 0 ; x < a.length ; x += 1) {
-      if (a[x] == target) {
-        return x;
+
+
+    public static int linSearch(int[] a, int target) {
+      for (int x = 0 ; x < a.length ; x += 1) {
+        if (a[x] == target) {
+          return x;
+        }
+      }
+      return -1;
+    }
+
+    
+    public static int linSearchR(int[] a, int target) {
+      int leng = a.length - 1
+
+
+
+      
+      if (a[0] == target) {
+        return
       }
     }
-    return -1;
-  }
-  public static int linSearchR(int[] a, int target) {
-    int leng = a.length - 1
-    if (a[0] == target) {
-      return
+
+    public static int freq( int[] a, int target ) {
+      int answer = 0;
+      int counter = 0;
+      while (counter < a.length - 1) {
+        if (a[counter] == target) {
+          answer ++;
+        }
+        counter ++;
+      }
+      return answer;
     }
-  }
-  public static void main(String[] args) {
-    int[] local = arrayRandInts(5);
-    System.out.println(strArrayInts(local));
-    System.out.println(linSearch (local, 5));
-  }
+
+
+    public static void main(String[] args) {
+      int[] local = arrayRandInts(5);
+      System.out.println("Testing printing the string: " + strArrayInts(local));
+      System.out.println("Testing looking for an index of an int in the string: " + linSearch(local, 5));
+      System.out.println("Testing looking for a frequency of an int in the string: " + freq(local, 5));
+    }
 }
