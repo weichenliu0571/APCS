25a6bb8e1d61774e7803d478000d591af4ce7db8
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-11-13 12:55:53 -0500

    Update Loopier.java

--- 34_array/Loopier.java
+++ 34_array/Loopier.java
@@ -1,3 +1,19 @@
+/*
+Big Apples (Weichen Liu + Blueface, Jeffrey Tang + , Lior + )
+APCS
+HW34 -- Looping to find the index and frequency of a string
+2021-11-13
+time spent: .8 hours
+
+
+DISCO
+- 
+
+QCC
+-
+
+*/
+
   import java.lang.Math;
 
   public class Loopier {
@@ -30,17 +46,22 @@
       return -1;
     }
 
-    
+
     public static int linSearchR(int[] a, int target) {
-      int leng = a.length - 1
+      return helperSearchR(a , target, 0);
+  }
 
-
-
-      
-      if (a[0] == target) {
-        return
-      }
-    }
+   public static int helperSearchR(int[] a, int target, int counter) {
+     if (counter == a.length) { // returns -1 if target was not found in array
+       return -1;
+     }
+     else if (a[counter] == target) {
+       return counter; // returns  counter if target was found in array
+     }
+     else {
+       return helperSearchR(a, target, counter + 1); // recursion continues until target is found, or the array is completely parsed through
+     }
+   }
 
     public static int freq( int[] a, int target ) {
       int answer = 0;
@@ -54,11 +75,28 @@
       return answer;
     }
 
+    public static int freqR( int[] a, int target) {
+      return helperFreqR(a, target, 0, 0);
+    }
+
+    public static int helperFreqR(int[] a, int target, int counter, int freq) {
+      if (counter == a.length) {
+        return freq;
+      } else {
+        if (a[counter] == target) {
+          return helperFreqR(a, target, counter + 1, freq + 1);
+        }
+        return helperFreqR(a, target, counter + 1, freq);
+      }
+    }
+
 
     public static void main(String[] args) {
       int[] local = arrayRandInts(5);
       System.out.println("Testing printing the string: " + strArrayInts(local));
-      System.out.println("Testing looking for an index of an int in the string: " + linSearch(local, 5));
-      System.out.println("Testing looking for a frequency of an int in the string: " + freq(local, 5));
+      System.out.println("Testing looking for an index of an int(5) in the string: " + linSearch(local, 5));
+      System.out.println("Testing looking for an index of an int(5) in the string using recursion: " + linSearchR(local, 5));
+      System.out.println("Testing looking for a frequency of an int(5) in the string: " + freq(local, 5));
+      System.out.println("Testing looking for a frequency of an int(5) in the string using recursion: " + freqR(local, 5));
     }
 }
