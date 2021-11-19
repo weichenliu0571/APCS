dc7cb607aa138ff8f4784ce2a75511c152001617
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-11-19 12:26:23 -0500

    Update Protagonist.java

--- 39_yoRPG/Protagonist.java
+++ 39_yoRPG/Protagonist.java
@@ -1,10 +1,68 @@
 public class Protagonist {
   private String name; 
+  private int hp;
+  private int defense;
+  private int stength;
+  
+
+    
+    
   public Protagonist() {
+    name = "Pat";
+    hp = 200;
+    defense = 10;
+    strength = 20;
   }
   
   public Protagonist( String setName) {
     name = setName;
   }
+  
+  public boolean isAlive(){
+    if (hp > 0) {
+      return true;
+  }
+    return false;
+  }
+  
+  
+  //accessing instance variable
+  
+  public String getName () {
+    return name;
+  }
+  
+  public int getHP () {
+    return hp;
+  }
+  
+  public int getDefense () {
+    return defense;
+  }
+  
+  public int getStrength () {
+    return strength; 
+  }
+  
+ 
+  
+  //Changing instance variables
+  
+  public setName (int newName) {
+    name = Newname;
+  }
+  
+  public setHP (int newHP) {
+    hp = newHP;
+  }
+  
+  public setDefense (int newDefense) {
+    return defense;
+  }
+  
+  public setStrength (int newStrength) {
+    return strength; 
+  }
+  
   
 }
