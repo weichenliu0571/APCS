b8b674b52cc13b6218084cddbe440722666268a2
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-11-19 12:12:48 -0500

    Update Monster.java

--- 39_yoRPG/Monster.java
+++ 39_yoRPG/Monster.java
@@ -1,8 +1,14 @@
 public class Monster {
+  private int hp;
+  
   public Monster() {
-    
-    
-    
+  }
+  
+  public boolean isAlive(){
+    if (hp > 0) {
+      return true;
+  }
+    return false;
   }
   
   
