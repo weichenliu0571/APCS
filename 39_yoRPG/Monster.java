557d352de0d711c5e1ee0390e97a7a130c7779a0
Weichen <wliu30@stuy.edu>
2021-11-21 17:09:11 -0500

    a

--- 39_yoRPG/Monster.java
+++ 39_yoRPG/Monster.java
@@ -1,46 +1,61 @@
 public class Monster {
   private int hp;
   private int defense;
-  private int strength;
-  
+  private int atk;
+
   public Monster() {
+    hp = 200;
+    defense = 50;
+    atk = 60;
   }
-  
+
+  public void bleed(int dmg) {
+    hp -= dmg; 
+  }
+
+  public int attack(Protagonist protagonist) {
+    int damage = atk - defense;
+    if (damage > 0) {
+      protagonist.bleed(damage);
+    }
+    return damage;
+  }
+
   public boolean isAlive(){
     if (hp > 0) {
       return true;
   }
     return false;
   }
-  
+
   // accessing instance variables
-  
+
   public int getHp () {
     return hp;
   }
-  
+
   public int getDefense() {
     return defense;
   }
-  
-  public int getStrength() {
-    return strength;
+
+  public int getATK() {
+    return atk;
   }
-  
-  
-  
-  
+
+
+
+
   // changing instance variables
-  
+
   public int setHP(int newHP) {
     hp = newHP;
   }
-  
+
   public int setDefense(int newDefense) {
     defense = newDefense;
   }
-  
-  public int setStrength(int newStrength) {
-    strength = newStrength; 
+
+  public int setATK(int newATK) {
+    atk = newATK;
   }
 }
--- 39_yoRPG/Protagonist.java
+++ 39_yoRPG/Protagonist.java
@@ -1,76 +1,93 @@
 public class Protagonist {
-  private String name; 
+  private String name;
   private int hp;
   private int defense;
-  private int stength;
-  
+  private int atk;
+  private int specialpower;
 
-    
-    
+
+
+
   public Protagonist() {
     name = "Pat";
     hp = 200;
     defense = 10;
-    strength = 20;
+    atk = 20;
+    specialpower = Math.random() * 15;
   }
-  
+
   public Protagonist( String setName) {
+    this()
     name = setName;
   }
-  
-  
+
+
   // methods
-  
+
   public boolean isAlive(){
     if (hp > 0) {
       return true;
   }
     return false;
   }
-  
-  public void attack(Monster smaug) {
-    int newHP = smaug.getHP() - 
-    smaug.setHP(newHP);
 
+  public void bleed(int dmg) {
+    hp -= dmg;
   }
-  
+
+  public void specialize() {
+    atk += specialpower;
+  }
+
+  public void normalize() {
+    def += specialpower; 
+  }
+
+  public int attack(Monster monster) {
+    int damage = atk - defense;
+    if (damage > 0) {
+      monster.bleed(damage);
+    }
+    return damage;
+  }
+
   //accessing instance variable
-  
+
   public String getName () {
     return name;
   }
-  
+
   public int getHP () {
     return hp;
   }
-  
+
   public int getDefense () {
     return defense;
   }
-  
-  public int getStrength () {
-    return strength; 
+
+  public int getATK () {
+    return atk;
   }
-  
- 
-  
+
+
+
   //Changing instance variables
-  
+
   public setName (int newName) {
     name = Newname;
   }
-  
+
   public setHP (int newHP) {
     hp = newHP;
   }
-  
+
   public setDefense (int newDefense) {
     return defense;
   }
-  
-  public setStrength (int newStrength) {
-    return strength; 
+
+  public setStrength (int newATK) {
+    return atk;
   }
-  
-  
+
+
 }
