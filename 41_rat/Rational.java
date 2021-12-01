1af83cc9fd7f821035a448f1a2c99074b74b3b3c
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-12-01 17:58:59 -0500

    Update Rational.java

--- 41_rat/Rational.java
+++ 41_rat/Rational.java
@@ -1,1 +1,46 @@
+/*****************************************************
+Big Apples (Weichen Liu + Blueface, Jeffery Tang + Mathias, Lior + Toothless)
+APCS pd6
+HW41 -- Rational
+2021-12-1
+time spent: 0.5 hours
 
+DISCO
+-
+QCC
+-
+ *****************************************************/
+
+ public class Rational {
+   private int numerator;
+   private int denominator;
+
+   public Rational() {
+     numerator = 0;
+     denominator = 1;
+   }
+   public Rational(int p, int q) {
+     if (q == 0) {
+       System.out.println("0 IS NOT A VALID DENOMINATOR FOR A RATIONAL NUMBER!!!");
+       this();
+     } else {
+     numerator = p;
+     denominator = q;
+     }
+   }
+
+   public float floatValue() {
+     return (float) numerator / (float) denominator;
+   }
+   public String toString() {
+     return "" + numerator + "/" + denominator;
+   }
+   public void multiply(Rational a) {
+     numerator = numerator * a.numerator;
+     denominator = denominator * a.denominator; 
+   }
+   public void divide(Rational a) {
+     numerator = numerator * a.denominator;
+     denominator = denominator * a.numerator; 
+   }
+ }
