243a9fd54e626ea1f470c6d0c81048e1e1ac7e88
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-12-04 20:04:24 -0500

    Update Rational.java

--- 42_rat/Rational.java
+++ 42_rat/Rational.java
@@ -90,14 +90,14 @@ public class Rational
   { 
     int n = this._numerator; 
     this._numerator = this._numerator * r._denominator + r._numerator * this._denominator; 
-    this._denominator = this._denominator * n._denominator;   
+    this._denominator = this._denominator * n;   
   }
   
   public void subtract(Rational r) 
   { 
-    int n = this._numerator; 
+    int n = this._denominator; 
     this._numerator = this._numerator * r._denominator - r._numerator * this._denominator; 
-    this._denominator = this._denominator * n._denominator;   
+    this._denominator = this._denominator * n;   
   }
   
   public static int gcd(int a, int b) {
