a961f1938b18970521fe33db4b9522676fb8e8e8
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-12-08 18:37:53 -0500

    Update ListInt.java

--- 45_suparr/ListInt.java
+++ 45_suparr/ListInt.java
@@ -1,1 +1,23 @@
+/*****************************************************
+Big Apples (Weichen Liu + Blueface, Jeffery Tang + Mathias, Lior + Toothless)
+APCS pd6
+HW45 -- interface for SuperArray
+2021-12-07
+time spent:  0.5 hours
 
+DISCO
+- There should be no body for the methods in the interface. 
+- The syntax for the class that implements the interface is [accessibility] [class] [className] implements [interfaceName] 
+QCC
+- Is there a situation where the methods in the interface should be private? 
+ *****************************************************/
+
+public interface ListInt {
+  public int size();
+  public int set(int index, int newVal); 
+  public int get( int index ); 
+  
+  public void add( int newVal);
+  public void add( int index, int newVal);
+  public void remove( int index); 
+}
