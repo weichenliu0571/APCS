1b908d930da759d102ba71c4045810eb7c25fead
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-12-15 20:01:26 -0500

    Update LinSearch.java

--- 48_search/LinSearch.java
+++ 48_search/LinSearch.java
@@ -5,9 +5,9 @@ HW48 -- Linear & Binary Search
 2021-12-15
 time spent:  0.5 hours
 DISCO
--
+- We should use compareTo method to compare the elements on the list. 
 QCC
--
+- What is an unchecked call?
  *****************************************************/
 
 /**
