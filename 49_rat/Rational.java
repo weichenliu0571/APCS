3502872541ec025d5c82400662a168062d7364fe
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-12-22 19:47:35 -0500

    Update Rational.java

--- 49_rat/Rational.java
+++ 49_rat/Rational.java
@@ -7,7 +7,8 @@ HW49 -- Java Conventions
 time spent:  0.5 hours
 
 DISCO
-- We can use throw new ClassCastException() to throw an error. 
+- We can use "throw new ClassCastException()" to throw an error. 
+- We should use boolean short circuiting to get out if the object is not of class Rational. 
 
 QCC
 - Do we have to implement Comparable?
