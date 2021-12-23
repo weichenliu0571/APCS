497c9402ec2841904f8da14609a487b2a124f521
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-12-22 19:49:18 -0500

    Update Driver.java

--- 49_rat/Driver.java
+++ 49_rat/Driver.java
@@ -1,11 +1,27 @@
+/*****************************************************
+Big Apples (Weichen Liu + Blueface, Jeffery Tang + Mathias, Lior + Toothless)
+APCS pd6
+HW49 -- Java Conventions
+2021-12-22
+time spent:  0.5 hours
+DISCO
+- We can use "throw new ClassCastException()" to throw an error. 
+- We should use boolean short circuiting to get out if the object is not of class Rational. 
+QCC
+- Do we have to implement Comparable?
+ *****************************************************/
+
 public class Driver {
   public static void main(String[] args) {
     Rational a = new Rational(3,5);
-    Rational b = new Rational(6,10);
+    Rational b = new Rational(6,11);
+    Rational d = new Rational(4,5);
     Integer c = new Integer(5); 
     
     // This should run
     System.out.println(a.compareTo(b)); 
+    System.out.println(a.compareTo(c));
+    System.out.println(a.compareTo(d)); 
     
     // This should return error
     System.out.println(b.compareTo(c)); 
