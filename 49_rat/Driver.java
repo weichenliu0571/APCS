6667bb08b0897dc70f4d4a0dec1c5fca25739e2f
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2021-12-22 19:34:00 -0500

    Update Driver.java

--- 49_rat/Driver.java
+++ 49_rat/Driver.java
@@ -1,1 +1,18 @@
-
+public class Driver {
+  public static void main(String[] args) {
+    Rational a = new Rational(3,5);
+    Rational b = new Rational(6,10);
+    Integer c = new Integer(5); 
+    
+    // These should run
+    System.out.println(a.compareTo(b)); 
+    
+    // These should return error
+    System.out.println(c.compareTo(b)); 
+    
+  }
+  
+  
+  
+  
+}
