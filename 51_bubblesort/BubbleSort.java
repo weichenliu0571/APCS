53c779fbd4fa0b0adaf0e456ab7f58a7a5458107
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2022-01-03 18:37:13 -0500

    Update BubbleSort.java

--- 51_bubblesort/BubbleSort.java
+++ 51_bubblesort/BubbleSort.java
@@ -64,7 +64,7 @@ public class BubbleSort
       // Amount of times we pass through the entire arraylist
       for ( int x = data.size() - 1 ; x > 0 ; x--) {
         // Allows us to compare consecutive values of elements on the array list
-        if (data[x].compareTo(data[x-1] < 0)) { 
+        if (data.get(x).compareTo(data.get(x-1)) < 0)) { 
           Comparable n = data[x - 1];
           data.set(x, data.get(x-1));
           data.set(x-1, n);
