c142b83069e5c8eb44c656d848b590529d5dfdac
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2022-02-13 20:18:19 -0500

    Update Review.java

--- cobo/review/Review.java
+++ cobo/review/Review.java
@@ -5,6 +5,14 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.io.*;
 
+/*
+Team Suspicious Oranges: Kevin Li, Hamim Seam, Weichen Liu
+APCS
+Lab 6 -- College Board Consumer Review Lab
+2022-02-13
+time spent: 2 hours
+*/
+
 /**
  * Class that contains helper methods for the Review Lab
  **/
