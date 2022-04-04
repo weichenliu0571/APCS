369ba78530f887e3ce49d39edbc2ae186645d5ec
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2022-04-04 18:49:05 -0400

    Update Queue.java

--- 88_rqueue/Queue.java
+++ 88_rqueue/Queue.java
@@ -1,112 +1,25 @@
 /***
- * class RQueue
- * SKELETON
- * A node-based, randomized queue
- * (a collection with FIIDKO property)
- *
- *       -------------------------------
- *   end |  --->   Q U E U E   --->    | front
- *       -------------------------------
- *
- *  linkages point opposite direction for O(1) en/dequeuing
- *            N <- N <- ... <- N <- N
- *      _end -^                     ^- _front
- *
+ * interface Queue
+ * Includes only methods listed in AP subset of Java
+ * (Local version, to override built-in Java implementation.)
  **/
 
-
-public class RQueue<SWASHBUCKLE> implements Queue<SWASHBUCKLE>
+public interface Queue<QUASAR>
 {
-  //instance variables
-  private LLNode<SWASHBUCKLE> _front, _end;
-  private int _size;
+  //~~~~~~~~~~~~~~~~~~begin AP subset~~~~~~~~~~~~~~~~~~
+  //means of removing an element from collection:
+  //Dequeues and returns the first element of the queue.
+  public QUASAR dequeue();
 
+  //means of adding an element to collection:
+  //Enqueue an element onto the back of this queue.
+  public void enqueue( QUASAR x );
 
-  // default constructor creates an empty queue
-  public RQueue()
-  {
+  //Returns true if this queue is empty, otherwise returns false.
+  public boolean isEmpty();
 
-  }
+  //Returns the first element of the queue without dequeuing it.
+  public QUASAR peekFront();
+  //~~~~~~~~~~~~~~~~~~~end AP subset~~~~~~~~~~~~~~~~~~~
 
-
-  public void enqueue( SWASHBUCKLE enQVal )
-  {
-
-  }//O(?)
-
-
-  // remove and return thing at front of queue
-  // assume _queue ! empty
-  public SWASHBUCKLE dequeue()
-  {
-
-  }//O(?)
-
-
-  public SWASHBUCKLE peekFront()
-  {
-
-  }//O(?)
-
-
-  /***
-   * void sample() -- a means of "shuffling" the queue
-   * Algo:
-   *   < YOUR SUCCINCT SUMMARY HERE >
-   **/
-  public void sample ()
-  {
-
-  }//O(?)
-
-
-  public boolean isEmpty()
-  {
-    return _front == null;
-  } //O(?)
-
-
-  // print each node, separated by spaces
-  public String toString()
-  {
-
-  }//end toString()
-
-
-
-  //main method for testing
-  public static void main( String[] args )
-  {
-
-      /*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
-    
-    Queue<String> PirateQueue = new RQueue<String>();
-
-    System.out.println("\nnow enqueuing..."); 
-    PirateQueue.enqueue("Dread");
-    PirateQueue.enqueue("Pirate");
-    PirateQueue.enqueue("Roberts");
-    PirateQueue.enqueue("Blackbeard");
-    PirateQueue.enqueue("Peter");
-    PirateQueue.enqueue("Stuyvesant");
-
-    System.out.println("\nnow testing toString()..."); 
-    System.out.println( PirateQueue ); //for testing toString()...
-
-    System.out.println("\nnow dequeuing..."); 
-    System.out.println( PirateQueue.dequeue() );
-    System.out.println( PirateQueue.dequeue() );
-    System.out.println( PirateQueue.dequeue() );
-    System.out.println( PirateQueue.dequeue() );
-    System.out.println( PirateQueue.dequeue() );
-    System.out.println( PirateQueue.dequeue() );
-
-    System.out.println("\nnow dequeuing fr empty queue...\n" +
-                       "(expect NPE)\n"); 
-    System.out.println( PirateQueue.dequeue() );
-
-      ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
-
-  }//end main
-
-}//end class RQueue
+}//end interface Queue
