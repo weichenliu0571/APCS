cdd3b5ba6215e39718d380878b8574e1c70bdb65
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2022-04-13 19:54:42 -0400

    Update ListDeque.java

--- 90_deque/ListDeque.java
+++ 90_deque/ListDeque.java
@@ -1,21 +1,28 @@
+// Mister George, Ziying, Diana, Weichen
+
 import java.util.*;
 import java.lang.*;
 
 public class ListDeque<Card> implements Deque<Card>{
 
-private LinkedList<Card> _arr;
+private ArrayList<Card> _arr;
 
+// constructor that initalizes a new AL
 public ListDeque(){
-    _arr = new LinkedList<Card>();
+    _arr = new ArrayList<Card>();
 }
 
+// inserts the specified element 
+// at the front of this deque if it is possible to do so
 public void addFirst(Card e){
     if (e == null){
         throw new NullPointerException();
     }
-    _arr.add(_arr.size()-1, e);
+    _arr.add(0, e);
 }
 
+// inserts the specified element 
+// at the end of this deque if it is possible to do so
 public void addLast(Card e){
     if (e == null){
         throw new NullPointerException();
@@ -23,6 +30,8 @@ public void addLast(Card e){
     _arr.add(e);
 }
 
+// removes the specified element 
+// at the front of this deque if it is possible to do so
 public Card removeFirst(){
     if (_arr.size() < 1){
         throw new NoSuchElementException();
@@ -30,6 +39,8 @@ public Card removeFirst(){
     return _arr.remove(0);
 }
 
+// removes the specified element 
+// at the end of this deque if it is possible to do so
 public Card removeLast(){
     if (_arr.size() < 1){
         throw new NoSuchElementException();
@@ -37,6 +48,7 @@ public Card removeLast(){
     return _arr.remove(_arr.size()-1);
 }
 
+// returns the first element of the deque
 public Card peekFirst(){
     if (_arr.size() < 1){
         return null;
@@ -44,6 +56,7 @@ public Card peekFirst(){
     return _arr.get(0);
 }
 
+// returns the last element of the deque
 public Card peekLast() {
     if (_arr.size() < 1){
         return null; 
@@ -51,6 +64,7 @@ public Card peekLast() {
     return _arr.get(_arr.size()-1);
 }
 
+// toString() method
 public String toString(){
     return _arr.toString();
 }
