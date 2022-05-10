8b093d8bee2c9e93b165649a0ace153eedfeb76f
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2022-05-09 21:30:28 -0400

    Update TreeNode.java

--- 95_bst/TreeNode.java
+++ 95_bst/TreeNode.java
@@ -1,4 +1,8 @@
-// Yatlongstan :: Gabriel, Yat Long, Weichen 
+//Yatlongstan :: Gabriel, Yat Long, Weichen 
+//APCS 
+//HW95 -- Algorithm as Data Structure 
+//2022-05-09m 
+//time spent: 0.8 hrs
 
 /**
  * class TreeNode
