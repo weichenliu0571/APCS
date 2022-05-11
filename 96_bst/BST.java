fecebe049ee34f33e6339f9e1b7b11bcc2ce7eba
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2022-05-10 20:15:53 -0400

    Update BST.java

--- 96_bst/BST.java
+++ 96_bst/BST.java
@@ -1,8 +1,8 @@
-//Yatlongstan :: Gabriel, Yat Long, Weichen 
-//APCS 
-//HW96 -- BSTs is the Perfect Place for Shade
-//2022-05-10t
-//time spent: 0.8 hrs
+// Team Yatlongstan (Yat Long Chan, Weichen Liu, Gabriel Thompson)
+// APCS
+// HW96 -- BSTs is the Perfect Place for Shade
+// 2022-05-10
+// time spent: 0.8 hrs
 
 /**
  * class BST
@@ -41,30 +41,31 @@ public class BST
   public void insert( int newVal )
   {
     TreeNode newNode = new TreeNode( newVal );
-    if (_root == null) {
+
+    if ( _root == null ) {
       _root = newNode;
       return;
     }
-    insert(_root, newNode);
+    insert( _root, newNode );
   }
   //recursive helper for insert(int)
   public void insert( TreeNode stRoot, TreeNode newNode )
   {
-    if (newNode.getValue() < stRoot.getValue()) {
-      if (stRoot.getLeft() == null) { // check if left child is empty
-        stRoot.setLeft(newNode);
-      }
-      else { // process down
-        insert(stRoot.getLeft(), newNode);
-      }
+    if ( newNode.getValue() < stRoot.getValue() ) {
+      //if no left child, make newNode the left child
+      if ( stRoot.getLeft() == null )
+        stRoot.setLeft( newNode );
+      else //recurse down left subtree
+        insert( stRoot.getLeft(), newNode );
+      return;
     }
-    else if (newNode.getValue() > stRoot.getValue()){ // newNode.getValue() > stRoot.getValue()
-      if (stRoot.getRight() == null) { // check if right child is empty
-        stRoot.setRight(newNode);
-      }
-      else { // process down
-        insert(stRoot.getRight(), newNode);
-      }
+    else { // new val >= curr, so look down right subtree
+      //if no right child, make newNode the right child
+      if ( stRoot.getRight() == null )
+        stRoot.setRight( newNode );
+      else //recurse down right subtree
+        insert( stRoot.getRight(), newNode );
+      return;
     }
   }//end insert()
 
@@ -84,18 +85,11 @@ public class BST
   }
   public void preOrderTrav( TreeNode currNode )
   {
-    //base case: at the end of tree
-    if (currNode == null) {
-      return;
-    }
-    //process currNode
-    System.out.println(currNode.getValue());
-
-    //process left subtree
-    preOrderTrav(currNode.getLeft());
-
-    //process right subtree
-    preOrderTrav(currNode.getRight());
+    if ( currNode == null )
+	    return;
+    System.out.print( currNode.getValue() + " " );
+    preOrderTrav( currNode.getLeft() );
+    preOrderTrav( currNode.getRight() );
   }
 
   //recurse left, process root, recurse right
@@ -105,48 +99,101 @@ public class BST
   }
   public void inOrderTrav( TreeNode currNode )
   {
-    //base case: at the end of tree
-    if (currNode == null) {
+    if ( currNode == null )
       return;
-    }
-
-    //process left subtree
-    inOrderTrav(currNode.getLeft());
-
-    //process currNode
-    System.out.println(currNode.getValue());
-
-    //process right subtree
-    inOrderTrav(currNode.getRight());
+    inOrderTrav( currNode.getLeft() );
+    System.out.print( currNode.getValue() + " " );
+    inOrderTrav( currNode.getRight() );
   }
-  
 
   //recurse left, recurse right, process root
   public void postOrderTrav()
   {
-    postOrderTrav(_root);
+    postOrderTrav( _root );
   }
   public void postOrderTrav( TreeNode currNode )
   {
-    //base case: at the end of tree
-    if (currNode == null) {
+    if ( currNode == null )
       return;
-    }
-
-    //process left subtree
-    postOrderTrav(currNode.getLeft());
-
-    //process right subtree
-    postOrderTrav(currNode.getRight());
-    
-    //process currNode
-    System.out.println(currNode.getValue());
-    
+    postOrderTrav( currNode.getLeft() );
+    postOrderTrav( currNode.getRight() );
+    System.out.print( currNode.getValue() + " "  );
   }
 
   //~~~~~~~~~~~~~^~~TRAVERSALS~~^~~~~~~~~~~~~~~~~~~~~~
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  //~~~~~~~~~~~~~v~~~COUNTERS~~v~~~~~~~~~~~~~~~~~~~~~
+
+    /*****************************************************
+     * TreeNode search(int)
+     * returns pointer to node containing target,
+     * or null if target not found
+     *****************************************************/
+    TreeNode search( int target )
+    {
+        return searchHelper(_root, target);
+    }
+
+    TreeNode searchHelper( TreeNode currNode, int target ) {
+	if (currNode == null) {
+		return null;
+	}
+
+        if (currNode.getValue() < target) {
+		return searchHelper(currNode.getRight(), target);
+	} else if (currNode.getValue() > target) {
+		return searchHelper(currNode.getLeft(), target);
+	} else {
+		return currNode;
+	}
+   }
+
+
+    /*****************************************************
+     * int height()
+     * returns height of this tree (length of longest leaf-to-root path)
+     * eg: a 1-node tree has height 1
+     *****************************************************/
+    public int height()
+    {
+        return heightHelper(_root);
+    }
+
+    // recursive case:   (in the case where you're not at a null node) return max(height(left), height(right)) + 1
+    // base case:        (in the case where you ARE at a null node)    return 0
+    public int heightHelper(TreeNode currentNode) {
+	if (currentNode == null) {
+	    return 0;
+	} else {
+	    return Math.max(heightHelper(currentNode.getLeft()),
+                            heightHelper(currentNode.getRight())) + 1;
+	}
+    }
+
+
+    /*****************************************************
+     * int numLeaves()
+     * returns number of leaves in tree
+     *****************************************************/
+    // recursive case: 
+    // base case: you're at a leaf (return 1)
+    public int numLeaves()
+    {
+	return numLeavesHelper( _root );
+    }
+
+    public int numLeavesHelper(TreeNode currentNode) {
+        if (currentNode == null)
+	    return 0;
+
+	if (currentNode.getLeft() == null && currentNode.getRight() == null) {
+	    return 1;
+	} else {
+	    return numLeavesHelper(currentNode.getLeft()) + numLeavesHelper(currentNode.getRight());
+	}
+    }
 
   //main method for testing
   public static void main( String[] args )
@@ -176,6 +223,15 @@ public class BST
       arbol.postOrderTrav();
 
       System.out.println( "\n-----------------------------");
+
+      TreeNode currentTest;
+      for (int i = 1; i <= 6; i++) {
+        currentTest = arbol.search(i);
+        System.out.println(currentTest.getValue() + " ...should be \"" + i + "\"");
+      }
+
+      System.out.println("\n" + arbol.height() + " ...should be \"3\"");
+      System.out.println(arbol.numLeaves() + " ...should be \"3\"");
     /*~~~~~~~~~~~~move~me~down~~~~~~~~~~~~~~~~~~~~~~
       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
   }
